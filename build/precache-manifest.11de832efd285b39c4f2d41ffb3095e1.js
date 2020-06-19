af3c18af32617806ea2799d4a5492febc3a09611
Eduardo Wegner <eduardo.wegner@khomp.com>
2020-06-19 06:00:47 -0300

    inserido build do frontend, minor fixes

---
+++ build/precache-manifest.11de832efd285b39c4f2d41ffb3095e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b966a4a1568313f3592d6e3247247b83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b487994e2b4359eb24db",
+    "url": "/static/css/2.35045243.chunk.css"
+  },
+  {
+    "revision": "4f030fb1fbccef1d820b",
+    "url": "/static/css/main.22963131.chunk.css"
+  },
+  {
+    "revision": "b487994e2b4359eb24db",
+    "url": "/static/js/2.c0855fb5.chunk.js"
+  },
+  {
+    "revision": "59a0a07934ba5e7c0fd5281c0f1901d4",
+    "url": "/static/js/2.c0855fb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f030fb1fbccef1d820b",
+    "url": "/static/js/main.c45c8049.chunk.js"
+  },
+  {
+    "revision": "c8847a7997cf2ee5c46d",
+    "url": "/static/js/runtime-main.71af9b60.js"
+  }
+]);
--- routes/main_route.js
+++ routes/main_route.js
@@ -1,15 +1,25 @@
 'use strict';
-
+const express = require('express'),
+    path = require('path'),
+    appDir = path.dirname(require.main.filename);
 module.exports = function (control) {
-    app.route('/')
-        .get((req, res) => {
-                res.status(200).json("That's working, woorkiiiiing")
-        })
+    app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+    console.log(appDir)
+    app.use('/', express.static(appDir+'/build'));
+
+    // app.route('/')
+    //     .get((req, res) => {
+    //             res.status(200).json("That's working, woorkiiiiing")
+    //     })
     app.route('/api/users')
         .get((req, res) => {
             console.log(req.query.since)
             control.data_retrieve('/users?since=' + req.query.since).then(async (resp) => {
-                res.status(200).json({ 'data': resp, next_link: '/api/users?since=' + (parseInt(req.query.count)+30)})
+                res.status(200).json({ 'data': resp, next_link: '/api/users?since=' + (parseInt(req.query.since)+30)})
             })            
         })
     app.route('/api/users/:username/details')
